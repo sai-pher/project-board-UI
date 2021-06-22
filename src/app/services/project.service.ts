71bd170a576bed9d7828be696b057c0e2d91aa59
sai-pher <arimanwd@gmail.com>
2021-06-22 23:13:49 +0000

    switched route baseUrl to heroku server

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'http://localhost:8080/api/projects';
+const baseUrl = 'https://saipher-project-board-server.herokuapp.com';
 
 @Injectable({
   providedIn: 'root'
