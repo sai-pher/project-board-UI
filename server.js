31e4c98e274c9afa8c948e5b0532bab1baaa6bb2
sai-pher <arimanwd@gmail.com>
2021-06-23 02:11:46 +0000

    changed server file

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("./dist/project-board-ui"));
+app.use(express.static(__dirname + '/dist/project-board-ui'));
 
-app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/project-board-ui" });
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/project-board-ui/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
