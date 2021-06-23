22b1724764cee32a5e61dcc90756875f53bf1c6b
sai-pher <arimanwd@gmail.com>
2021-06-23 00:18:05 +0000

    changed project name

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("./dist/project-board-UI"));
+app.use(express.static("./dist/project-board-ui"));
 
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/project-board-UI" });
+  res.sendFile("index.html", { root: "dist/project-board-ui" });
 });
 
 app.listen(process.env.PORT || 8080);
