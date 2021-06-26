8ae3b83003420519d27a3847a71162e7f05364b5
sai-pher <arimanwd@gmail.com>
2021-06-26 14:15:42 +0000

    fixed server routing issue

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'https://saipher-project-board-server.herokuapp.com';
+const baseUrl = 'https://saipher-project-board-server.herokuapp.com/api/projects';
 
 @Injectable({
   providedIn: 'root'
